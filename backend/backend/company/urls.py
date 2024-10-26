41146dca56d90aaeedb2794780c75f11759256c9
L.V.C.Janith sirilal <janithcsv.com>
2024-10-26 23:37:42 +0530

    loging as admine with genarated id and passsword sucessfull

--- backend/backend/company/urls.py
+++ backend/backend/company/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
-from .views import register_company
+from .views import register_company ,admin_login
 
 urlpatterns = [
     path('check-mongo-connection/', views.check_mongo_connection, name='check_mongo_connection'),
     path('register-company/', register_company, name='register_company'),
+     path('admin-login/', views.admin_login, name='admin_login'),
 ]
+
+
+
+
